a71dff0efc419d786eef2a9563bf550ca235cb0b
tribagus <tribagus.indonesia@gmail.com>
2023-07-18 13:39:02 +0700

    fix: fix notfound error

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -9,8 +9,8 @@ export default function NotFound() {
       <div className="flex flex-col mx-auto items-center justify-center px-4">
         <h1 className="text-[100px] md:text-[130px] leading-none">404</h1>
         <p className="text-center text-lg md:text-xl max-w-lg font-thin">
-          Sorry.. We searched high and low but couldn't find what you're looking
-          for. Let's find a better place for you to go.
+          Sorry.. We searched high and low but couldn&apos;t find what
+          you&apos;re looking for. Let&apos;s find a better place for you to go.
         </p>
         <button
           onClick={() => router.back()}
